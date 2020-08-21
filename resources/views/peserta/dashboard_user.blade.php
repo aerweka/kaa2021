d86909efc7697bad39fcc75a5981dc4e72057167
Rizal Al Rosyid <69848492+RizalAlRosyid@users.noreply.github.com>
2020-08-21 23:39:27 +0700

    view dashboard dkk + auth
    auth nya bermasalah di css (klo browser di zoom atau un-zoom lebih dari 100% atau kurang)

---
+++ resources/views/peserta/dashboard_user.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="/css/dashboard_user.css">
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <tr>
+                <a href="/peserta/dashboard_user" class="menu1">Dashboard</a>
+                <a href="/peserta/alur_pembayaran" class="menu2">Alur Pembayaran</a>
+                <a href="/peserta/konfirmasi_pembayaran" class="menu3">Konfirmasi Pembayaran</a>
+                <a href="/peserta/form_pendaftaran" class="menu4">Form Pendaftaran</a>
+                <a href="/peserta/cetak_kartu_peserta" class="menu5">Cetak Kartu Peserta</a>
+            </tr>
+        </div>
+        <div class="conten">
+            <h1>Selamat Datang, User . .</h1>
+            <div class="bungkus">
+                <h2>PENGUMUMAN</h2>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora laborum cum beatae libero nam soluta corrupti quasi, assumenda doloribus impedit voluptate dolore vel harum minus accusamus cumque at dignissimos rerum? Accusamus alias illo nulla atque accusantium minus quo fugit amet.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore quas magni ducimus eligendi sit asperiores possimus recusandae laborum vel cumque eum labore accusantium velit aspernatur distinctio ex, non in facilis modi quos fugit sequi rem soluta illo. Harum, non repudiandae maxime pariatur tempore rem eum unde alias eius vel eveniet suscipit nisi quo quod, natus voluptatum beatae neque, omnis cupiditate error totam. Magnam quis sapiente aut impedit odio perferendis ea!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, perferendis molestias! Ut doloremque, temporibus dolor laudantium minima deleniti dolores voluptates!
+                    </p>
+            </div>
+            <!-- <div class="telpon">Phone Number</div> -->
+        </div>
+    </div>
+</body>
+</html>
---
+++ resources/views/register.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" type="text/css" href="/css/regist.css">
+</head>
+<body>
+    <div class="background">
+        <q class="lgs">Let's Get Started</q>
+        <form>
+            <div class="container">
+                    <h2 class="signin">REGISTER</h2>
+                    <input type="text" class="name" placeholder="Name">
+                    <br><br>
+                    <input type="text" class="univercity" placeholder="Univercity">
+                    <br><br>
+                    <input type="email" class="email" placeholder="Email">
+                    <br><br>
+                    <a type="submit" class="btn" href="/peserta/dashboard_user">Sign Up</a>
+                    <p>Already Have Account? <a href="/login">Click Here</p>
+                </div>
+            </form>
+        </div>
+</body>
+</html>
