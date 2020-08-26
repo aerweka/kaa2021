b156b7a47002c2dd23571dc781884cbcf86950a1
deaamartya <50395247+deaamartya@users.noreply.github.com>
2020-08-26 17:53:54 +0700

    logout fixed

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -45,11 +46,15 @@ class LoginController extends Controller
         return 'username';
     }
 
-    public function logout()
+    public function logout(Request $request)
     {
-        $this->doLogout($request);
+        $this->guard()->logout();
 
-        return redirect()->route('login');
+        $request->session()->flush();
+
+        $request->session()->regenerate();
+
+        return redirect('/login');
     }
 
 }
