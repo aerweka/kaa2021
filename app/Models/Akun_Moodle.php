90e9ec8b1341b88d1dafb227b418fca88f0e9323
Rizal Al Rosyid <69848492+RizalAlRosyid@users.noreply.github.com>
2020-10-15 12:11:14 +0700

    Add files via upload

---
+++ app/Models/Akun_Moodle.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Role
+ * 
+ * @property string $id_user
+ * @property string password_moodle
+ * 
+ * @property Collection|Pengguna[] $penggunas
+ *
+ * @package App\Models
+ */
+class Akun_Moodle extends Model
+{
+	protected $table = 'akun_moodle';
+	protected $primaryKey = 'id_user';
+	public $incrementing = false;
+	public $timestamps = false;
+
+	public function penggunas()
+	{
+		return $this->belongsTo(Pengguna::class, 'id_user');
+	}
+}
